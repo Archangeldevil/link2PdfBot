6730d201a97499a2c701916e13e7f4f3c121b2cb
Archangeldevil <90327941+Archangeldevil@users.noreply.github.com>
2022-02-25 09:21:45 +0530

    Update bot.py
    Start Command

--- bot.py
+++ bot.py
@@ -31,8 +31,8 @@ async def start_bot(self, m: Message):
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("🛡 Support Chat", url="t.me/rmprojects"),
-                 InlineKeyboardButton("🎯 Source", url="https://github.com/m4mallu")]
+                [InlineKeyboardButton("🛡 Support Chat", url="t.me/Cosmos_Discussion"),
+                 InlineKeyboardButton("🎯 Channel", url="https://t.me/TheShutenDoji")]
             ]
         )
     )
